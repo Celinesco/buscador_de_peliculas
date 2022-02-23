a2e19147eedb881af6ce5ce9ceb7edbde3e0a0ba
Celinesco <celina.alsina@gmail.com>
2022-02-23 17:41:04 +0100

    maquetado footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
     <div className="App">
       <NavBar />
+      <Footer />
     </div>
   );
 }
