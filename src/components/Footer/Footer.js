e4bbabcf65732524cf452b462ff374090b8c1256
Celinesco <celina.alsina@gmail.com>
2022-02-23 17:41:04 +0100

    maquetado footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,12 @@ import './Footer.scss';
 const Footer = () => {
     return (
         <footer className='footer'>
-
+            <p>Hecho por Celinesco</p>
+            <ul className='ul__footer'>
+                <li>GitHub</li>
+                <li>Linkedin</li>
+                <li>Instagram</li>
+            </ul>
         </footer>
     )
 }
