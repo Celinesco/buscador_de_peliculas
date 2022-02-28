88df406cc0934ee79294c2ad3cb37be1d296fe69
Celinesco <celina.alsina@gmail.com>
2022-02-28 12:31:43 +0100

    input seccion busqueda

--- src/components/SearchSection.js
+++ src/components/SearchSection.js
@@ -1,7 +1,14 @@
+import { useEffect, useState } from "react"
+import { useSearchParams } from "react-router-dom"
+
 const SearchSection = () => {
     return (
         <section className="section__search sections__styles">
             <h2>Search</h2>
+            <form>
+                <label htmlFor="movie-search">Search here!</label>
+                <input id="movie-search" type="text"></input>
+            </form>
         </section>
     )
 }
