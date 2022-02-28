10acf2ead6d5f17adf2961aae44d878180d2eb58
Celinesco <celina.alsina@gmail.com>
2022-02-28 11:44:48 +0100

    creo componente searchSection , agrego links a la barra de navegacion

--- src/components/MostPopularSection.js
+++ src/components/MostPopularSection.js
@@ -1,6 +1,6 @@
 const MostPopularSection = () => {
     return (
-        <section className="section__mostpopular">
+        <section className="section__mostpopular sections__styles">
             <h2>Most Popular</h2>
         </section>
     )
