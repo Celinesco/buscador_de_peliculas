b9d8a398863e58c008948cf996339ed5aaec14c9
Celinesco <celina.alsina@gmail.com>
2022-02-28 10:58:59 +0100

    iconos footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import './App.scss';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <NavBar />
-      <Footer />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" />
+          <Route path="/new-realeases"/>
+          <Route path="/popular" />
+          <Route path="/trending/page/:number-page" />
+          <Route path="/top_rated/page:number-page" />
+          <Route path="/upcoming/page/:number-page" />
+          <Route path="on_cinemas/page/:number-page" />
+          <Route path="/movie/:idMovie/info" />
+          <Route path="/movie/:idMovie/cast" />
+          <Route path="/movie/:idMovie/trailer" />
+          <Route path="/movie/:idMovie/similar-movies" />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+
     </div>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,18 @@
 import './Footer.scss';
+import { VscGithub, VscMail } from "react-icons/vsc";
+import { AiOutlineLinkedin } from "react-icons/ai";
 
 const Footer = () => {
     return (
         <footer className='footer'>
             <p>Hecho por Celinesco</p>
             <ul className='ul__footer'>
-                <li>GitHub</li>
-                <li>Linkedin</li>
-                <li>Instagram</li>
+                <li><a href="https://www.linkedin.com/in/celinesalsina/" target="_blank"
+                    class="icons-footer" aria-label="Perfil Linkedin de Celina"><AiOutlineLinkedin /></a></li>
+                <li><a href="https://github.com/Celinesco" target="_blank" class="icons-footer"
+                    aria-label="Enviar mail">< VscGithub /></a></li>
+                     <li><a href="mailto:celina.alsina@gmail.com" target="_blank" class="icons-footer"
+                    aria-label="Enviar mail a Celinesco">< VscMail /></a></li>
             </ul>
         </footer>
     )
