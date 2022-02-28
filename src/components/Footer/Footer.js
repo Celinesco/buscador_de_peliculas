ed889f705ef86e31092b326f8d6ea708c3b9d803
Celinesco <celina.alsina@gmail.com>
2022-02-28 13:21:11 +0100

    agrego rel a los links del footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,11 +7,11 @@ const Footer = () => {
         <footer className='footer'>
             <p>Hecho por Celinesco</p>
             <ul className='ul__footer'>
-                <li><a href="https://www.linkedin.com/in/celinesalsina/" target="_blank"
+                <li><a href="https://www.linkedin.com/in/celinesalsina/" target="_blank" rel='noreferrer noopener'
                     className="icons-footer" aria-label="Perfil Linkedin de Celina"><AiOutlineLinkedin /></a></li>
-                <li><a href="https://github.com/Celinesco" target="_blank" className="icons-footer"
+                <li><a href="https://github.com/Celinesco" target="_blank" rel='noreferrer noopener' className="icons-footer"
                     aria-label="Enviar mail">< VscGithub /></a></li>
-                     <li><a href="mailto:celina.alsina@gmail.com" target="_blank" className="icons-footer"
+                     <li><a href="mailto:celina.alsina@gmail.com" target="_blank" rel='noreferrer noopener' className="icons-footer"
                     aria-label="Enviar mail a Celinesco">< VscMail /></a></li>
             </ul>
         </footer>
