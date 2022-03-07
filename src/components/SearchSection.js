128c151bea88cbfbb24c0f62c2b7090008c52973
Celinesco <celina.alsina@gmail.com>
2022-03-07 17:22:41 +0100

    search section, agrego prop overwiev, import variables de api

--- src/components/SearchSection.js
+++ src/components/SearchSection.js
@@ -5,18 +5,15 @@ import Card from './Card';
 import './SectionSearch.scss';
 import posterNotFound from '../assets/posterNotFound.png'
 import ButtonPages from "./ButtonPages";
+import { API_KEY, URL_BASE, IMG_URL } from "./export_files";
 
 const SearchSection = () => {
-    const API_KEY = '65039781e8b8e09c46c6da646de7be01';
-    const BASE_URL = 'https://api.themoviedb.org/3/search/movie?';
-    const IMG_URL = 'https://image.tmdb.org/t/p/w500'
     const [search, setSearch] = useState([])
-    const [totalResults, setTotalResults] = useState(0)
     const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
     const [searchParams, setSearchParams] = useSearchParams({
         title_contains: "Mickey",
     });
+
     const [inputValue, setInputValue] = useState(searchParams.get('title_contains'));
 
     const handleOnChange = (e) => {
@@ -29,13 +26,11 @@ const SearchSection = () => {
         })
     }
     useEffect(() => {
-        fetch(`${BASE_URL}api_key=${API_KEY}&query=${searchParams.get('title_contains')}&page=${page}`)
+        fetch(`${URL_BASE}api_key=${API_KEY}&query=${searchParams.get('title_contains')}&page=${page}`)
             .then(res => res.json())
             .then(data => {
-                setTotalPages(data.total_pages)
                 setSearch(data.results ? data.results : [])
-                setTotalResults(data.total_results)
-
+            
             })
     }, [searchParams, page])
 
@@ -49,7 +44,6 @@ const SearchSection = () => {
                 <button type="submit" onClick={handleClick}><VscSearch /></button>
             </form>
             <div className="container__results">
-                <p className="p__results">Results: {totalResults}</p>
                 <div className="container__movie-cards">
                     {search.map((movie) => (
                         <Link to={`/movie/${movie.id}`} key={movie.id}>
@@ -61,6 +55,7 @@ const SearchSection = () => {
                                 alt={movie.poster_path !== null
                                     ? `Poster from ${movie.title}`
                                     : `Poster not available`}
+                                overview={movie.overview}
                             />
                         </Link>
                     )
@@ -69,7 +64,6 @@ const SearchSection = () => {
                 </div>
                 <ButtonPages 
                  page={page}
-                 totalPages={totalPages}
                  setPage={setPage}
                 />
             </div>
