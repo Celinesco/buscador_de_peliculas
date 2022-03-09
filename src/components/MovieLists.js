439ca97206440f74cb6a6d6eda0e1e9170d4d14e
Celinesco <celina.alsina@gmail.com>
2022-03-09 08:34:23 +0100

    estilado scroll-bar

--- src/components/MovieLists.js
+++ src/components/MovieLists.js
@@ -4,26 +4,72 @@ import { URL_BASE, API_KEY, IMGw300_URL } from './export_files';
 import ItemMovieList from './ItemMovieList';
 
 
-const MovieLists = ({endpoint, listTitle}) => {
+const MovieLists = ({ endpoint, listTitle }) => {
 
     const [list, setList] = useState([]);
 
-    useEffect (()=> {
+    useEffect(() => {
         fetch(`${URL_BASE}${endpoint}?api_key=${API_KEY}`)
-        .then(res => res.json())
-        .then(data =>(
-            setList(data.results)
-        ))
+            .then(res => res.json())
+            .then(data => (
+                setList(data.results)
+            ))
     }, [])
 
     return (
         <div className="container__movie-list">
-            <h3>{listTitle}</h3>
+            <div className='container__title-list'>
+                <div className='container__horizontal-square'>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                </div>
+                <div className='container__horizontal-square-bottom'>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                    <div className='horizontal-square'></div>
+                </div>
+                <h3>{listTitle}</h3>
+            </div>
             {list.map(movie => (
                 <ItemMovieList key={movie.id}
-                img={`${IMGw300_URL}${movie.poster_path}`}
-                title={movie.title}
-                idMovie={movie.id}/>
+                    img={`${IMGw300_URL}${movie.poster_path}`}
+                    title={movie.title}
+                    idMovie={movie.id} />
             ))}
         </div>
     )
