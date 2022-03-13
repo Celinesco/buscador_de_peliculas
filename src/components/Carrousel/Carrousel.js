399eb9102cc23a3453892c9664ef32a951022437
Celinesco <celina.alsina@gmail.com>
2022-03-13 18:10:49 +0100

    componente carrousel y componente contenedor carrousel, agrego variable de imagenes en export_files con tamanio original, hago fetch en contenedor carrousel

--- src/components/Carrousel/Carrousel.js
+++ src/components/Carrousel/Carrousel.js
@@ -1,13 +1,43 @@
+
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { IMGoriginal_URL } from '../../components/export_files';
+
+// Import Swiper styles
+import './Carrousel.scss';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import 'swiper/css/a11y';
+import 'swiper/css/scrollbar';
 
 
-const Carrousel = () => {
+
+const Carrousel = ({ info }) => {
+
     return (
-        <div>
+        <Swiper
+            modules={[Navigation, Pagination, Scrollbar, A11y]}
+            spaceBetween={50}
+            slidesPerView={1}
+            navigation
+            scrollbar={{ draggable: true }}
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+        >
+            {info?.map((movie) =>
+                <SwiperSlide key={movie.id}>
+                    <div className='container__img-slides'>
+                        <img src={`${IMGoriginal_URL}${movie.backdrop_path}`}></img>
+                    </div>
 
-        </div>
-    )
-}
+
+                </SwiperSlide>)}
+
+        </Swiper>
+    );
+};
 
 export default Carrousel;
--- src/components/export_files.js
+++ src/components/export_files.js
@@ -4,4 +4,6 @@ export const API_KEY = "65039781e8b8e09c46c6da646de7be01";
 
 export const IMGw500_URL = 'https://image.tmdb.org/t/p/w500';
 
-export const IMGw300_URL = 'https://image.tmdb.org/t/p/w300'
+export const IMGw300_URL = 'https://image.tmdb.org/t/p/w300';
+
+export const IMGoriginal_URL = 'https://image.tmdb.org/t/p/original';
