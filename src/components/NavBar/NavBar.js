daee41d9afc389b765d392339ee7513756ed517a
Celinesco <celina.alsina@gmail.com>
2022-03-15 17:49:14 +0100

    boton de menu hamburguesa

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,8 +2,15 @@ import './NavBar.scss';
 import { Link } from 'react-router-dom';
 import './NavBar.scss';
 import popCorn from '../../assets/popCorn.png';
+import { useState } from 'react';
 
 const NavBar = () => {
+    const [menuHamburguesa, setMenuHamburguesa] = useState(false)
+
+    const handleClick = () => {
+        setMenuHamburguesa(true)
+    }
+
     return (
         <nav className='navbar'>
             <div className='container__img-icon'>
@@ -16,6 +23,14 @@ const NavBar = () => {
                 <Link to="/search/"><li>SUCHE</li></Link>
                 <Link to="/movie/:idMovie"></Link>
             </ul>
+            <button onClick={handleClick} className='clappboard' >
+                <div className="punto"></div>
+                <div className={"upper-part-box " + `${menuHamburguesa && 'clap-clap'}`}></div>
+                <div className="square-clappboard">
+                    <div className="sub-square"></div>
+                    <p className="text-action-button">Aktion!</p>
+                </div>
+            </button>
         </nav>
     )
 }
