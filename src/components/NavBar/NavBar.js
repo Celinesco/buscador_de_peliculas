8140106bb2159e2c2405dc8b9c88b8b61f028534
Celinesco <celina.alsina@gmail.com>
2022-03-15 14:43:51 +0100

    estilado navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -10,10 +10,10 @@ const NavBar = () => {
                 <img src={popCorn} alt="popcorn, cotufas, palomitas de maiz, pororo, pururu"></img>
             </div>
             <ul className='ul__navbar'>
-                <Link to="/"><li>Start</li></Link>
-                <Link to="/newreleases"><li>Neuer- scheinungen</li></Link>
-                <Link to="/mostpopular"><li>Populär</li></Link>
-                <Link to="/search/"><li>Suche</li></Link>
+                <Link to="/"><li>START</li></Link>
+                <Link to="/newreleases"><li>NEU!</li></Link>
+                <Link to="/mostpopular"><li>POPULÄR</li></Link>
+                <Link to="/search/"><li>SUCHE</li></Link>
                 <Link to="/movie/:idMovie"></Link>
             </ul>
         </nav>
