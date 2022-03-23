8460ed5cff550372ce5ad87e148716128c3b7af9
Celinesco <celina.alsina@gmail.com>
2022-03-23 23:18:40 +0100

    Estilado funcion para cantidad de cuadrados en el swiper de las imagenes de abajo tamanio menu-navbar

---
+++ src/components/DecorativeSquares/DecorativeSquares.js
@@ -0,0 +1,10 @@
+import './DecorativeSquares.scss';
+
+
+const DecorativeSquares = (numberOfSquares) => {
+    return (
+        <div className='horizontal-square'></div>
+    )
+}
+
+export default DecorativeSquares;
